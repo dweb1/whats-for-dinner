01882aa10a4d32267fe323d3425eac3b6c6126b2
dweb1 <david.p.weber@gmail.com>
2017-07-27 16:12:51 -0400

    Edited app.js to run mongoose properly

--- app.js
+++ app.js
@@ -7,23 +7,15 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 require('dotenv').config();
-
-// Connect to database
-// if (process.env.MONGODB_URI) {
-//   mongoose.connect(process.env.MONGODB_URI);
-// }
-// else {
-// 	//change for link to actual database
-//   mongoose.connect('mongodb://localhost/express-movies');
-// }
-// mongoose.connection.on('error', function(err) {
-//   console.error('MongoDB connection error: ' + err);
-//   process.exit(-1);
-//   }
-// );
-// mongoose.connection.once('open', function() {
-//   console.log("Mongoose has connected to MongoDB!");
-// });
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 
 var index = require('./routes/index');
