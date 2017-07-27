71c5ea5ddedf75f362ba0796a1b39e5abd03a4e9
burnsbeaver <burnsbeaver@gmail.com>
2017-07-27 19:41:12 -0400

    Added Item Schema

--- db/schema.js
+++ db/schema.js
@@ -4,6 +4,12 @@ var Schema = mongoose.Schema;
 // Use native promises
 mongoose.Promise = global.Promise;
 
+var IngredientSchema = new Schema({
+  name: String,
+  category: String,
+  linkToRecipes: String
+});
+
 var RecipeSchema = new Schema({
   directions: String,
   servings: Number,
@@ -12,12 +18,15 @@ var RecipeSchema = new Schema({
   origin: String,
   difficulty: Number,
   picture: String,
-  //Change ingredients back to [IngredientsSchema]
+  //Change ingredients back to [IngredientSchema]
   ingredients: String
 });
 
+
 var RecipeModel = mongoose.model("Recipe", RecipeSchema);
+var IngredientModel = mongoose.model("Ingredient", IngredientSchema);
 
 module.exports = {
   Recipe: RecipeModel,
+  Ingredient: IngredientModel
 };
